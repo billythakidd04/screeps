41a37834f543c38ef3bb9905d910948b54100224
Bill Caffery <billythakidd04@gmail.com>
2020-05-09 23:18:02 -0400

    notes for more advanced ideas

---
+++ construction.js
@@ -0,0 +1,11 @@
+// Construction
+/* Roads
+How to find most direct path to objective?
+destX = 22
+destY = 22
+
+startX = 45
+startY = 21
+
+
+*/
---
+++ defense.js
@@ -0,0 +1,12 @@
+// defense module
+/*
+locate energy sources
+establish perimeter
+    1. block doors?
+    2. build near and direct to resources?
+build towers at:
+    1. spawn
+    2. energy sources
+build walls
+create soldiers
+*/
